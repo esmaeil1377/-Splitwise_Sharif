6eb083d2ccbff5e99ec13456322dd74a29c52cdb
mohammad-fattah <m.p.fattahpour@gmail.com>
2021-07-30 20:51:48 +0430

    add profile page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp';
 import LandingPage from './LandingPage';
+import Profile from './Profile';
 
 class Main extends Component {
   render() {
@@ -11,6 +12,7 @@ class Main extends Component {
         <Route path="/" exact component={LandingPage} />
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={SignUp} />
+        <Route path="/profile" component={Profile} />
       </div>
     );
   }
